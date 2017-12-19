5e2d9bcab6566b38ccfd5ad6e8bc95781620b2d9
ctrlnot <tonton.perocho@yahoo.com>
2017-12-19 18:05:09 +0800

    Fixed to 2 spaces.

--- js/cell.js
+++ js/cell.js
@@ -1,54 +1,54 @@
 function Cell(x, y) {
-    this.x = x;
-    this.y = y;
-    this.curr = 0;
-    this.prev = this.curr;
+  this.x = x;
+  this.y = y;
+  this.curr = 0;
+  this.prev = this.curr;
 }
 
 Cell.prototype.generate = function() {
-    // Save previous state
-    for (let x = 0; x < cols; x++) {
-        for (let y = 0; y < rows; y++) {
-            grid[x][y].prev = grid[x][y].curr;
-        }
+  // Save previous state
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      grid[x][y].prev = grid[x][y].curr;
     }
+  }
 
-    for (let x = 0; x < cols; x++) {
-        for (let y = 0; y < rows; y++) {
-            let g = grid[x][y];
-            let neighbors = 0;
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      let g = grid[x][y];
+      let neighbors = 0;
 
-            for (let i = -1; i <= 1; i++) {
-                for (let j = -1; j <= 1; j++) {
-                    neighbors += grid[(x + i + cols) % cols][(y + j + rows) % rows].prev;
-                }
-            }
-
-            neighbors -= g.prev; // Minus the current cell's state
-
-            // Rules of life
-            if (g.curr == 1 && neighbors < 2) g.curr = 0;            // Dies because of loneliness :(
-            else if (g.curr == 1 && neighbors > 3) g.curr = 0;       // Dies because of overpopulation :(
-            else if (g.curr == 0 && neighbors == 3) g.curr = 1;     // Reproduce
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          neighbors += grid[(x + i + cols) % cols][(y + j + rows) % rows].prev;
         }
+      }
+
+      neighbors -= g.prev; // Minus the current cell's state
+
+      // Rules of life
+      if (g.curr == 1 && neighbors < 2) g.curr = 0;            // Dies because of loneliness :(
+      else if (g.curr == 1 && neighbors > 3) g.curr = 0;       // Dies because of overpopulation :(
+      else if (g.curr == 0 && neighbors == 3) g.curr = 1;     // Reproduce
     }
+  }
 }
 
 Cell.prototype.hover = function(mx, my) {
-    var tmx = floor(mx / w);
-    var tmy = floor(my / w);
+  var tmx = floor(mx / w);
+  var tmy = floor(my / w);
 
-    if(tmx == this.x && tmy == this.y) {
-        stroke(255, 255, 0);
-    } else {
-        stroke(255, 50);
-    }
+  if(tmx == this.x && tmy == this.y) {
+    stroke(255, 255, 0);
+  } else {
+    stroke(255, 50);
+  }
 }
 
 Cell.prototype.show = function() {
-    if (this.prev == 0 && this.curr == 1) fill(255, 255, 0, 128);
-    else if (this.curr == 1) fill(255, 255, 0, 230);
-    else if (this.prev == 1 && this.curr == 0) fill(255, 255, 0, 50);
-    else noFill();
-    rect(this.x * w, this.y * w, w, w);
+  if (this.prev == 0 && this.curr == 1) fill(255, 255, 0, 128);
+  else if (this.curr == 1) fill(255, 255, 0, 230);
+  else if (this.prev == 1 && this.curr == 0) fill(255, 255, 0, 50);
+  else noFill();
+  rect(this.x * w, this.y * w, w, w);
 }
--- js/sketch.js
+++ js/sketch.js
@@ -11,53 +11,53 @@ var grid = [];
 var start = false;
 
 function setup() {
-    // createCanvas(1300, 650);
-    createCanvas(600, 600);
+  // createCanvas(1300, 650);
+  createCanvas(600, 600);
 
-    rows = floor(height / w);
-    cols = floor(width / w);
+  rows = floor(height / w);
+  cols = floor(width / w);
 
-    for (let x = 0; x < cols; x++) {
-        grid[x] = [];
-        for (let y = 0; y < rows; y++) {
-            grid[x][y] = new Cell(x, y);
-        }
+  for (let x = 0; x < cols; x++) {
+    grid[x] = [];
+    for (let y = 0; y < rows; y++) {
+      grid[x][y] = new Cell(x, y);
     }
+  }
 
-    frameRate(30);
+  frameRate(30);
 
-    // console.log((0 + (-1) + 10) % 10);
+  // console.log((0 + (-1) + 10) % 10);
 }
 
 function draw() {
-    background(51);
+  background(51);
 
-    for (let x = 0; x < cols; x++) {
-        for (let y = 0; y < rows; y++) {
-            if (start) grid[x][y].generate();
-            grid[x][y].hover(mouseX, mouseY);
-            grid[x][y].show();
-        }
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      if (start) grid[x][y].generate();
+      grid[x][y].hover(mouseX, mouseY);
+      grid[x][y].show();
     }
+  }
 
-    // console.log(start);
+  // console.log(start);
 }
 
 function mousePressed() {
-    if (mouseX <= width && mouseY <= height) {
-        let g = grid[floor(mouseX / w)][floor(mouseY / w)];
-        if (g.curr === 1) {
-            g.prev = g.curr;
-            g.curr = 0;
-        } else {
-            g.prev = g.curr;
-            g.curr = 1;
-        }
+  if (mouseX <= width && mouseY <= height) {
+    let g = grid[floor(mouseX / w)][floor(mouseY / w)];
+    if (g.curr === 1) {
+      g.prev = g.curr;
+      g.curr = 0;
+    } else {
+      g.prev = g.curr;
+      g.curr = 1;
     }
+  }
 }
 
 function keyPressed() {
-    if (key === ' ') {
-        start ? start = false : start = true;
-    }
+  if (key === ' ') {
+    start ? start = false : start = true;
+  }
 }
